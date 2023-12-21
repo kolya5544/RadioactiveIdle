92d65a945af42bf8a49c9dc090b22f8e27def2c7
kolya5544 <kolya@iktm.me>
2023-12-21 19:19:57 +0300

    fixed some minor spelling mistakes all around achievements.js

--- src/achievements/achievements.js
+++ src/achievements/achievements.js
@@ -71,7 +71,7 @@ Achievements.prototype = {
         this.addNewAchievement("New Beginnings", "Earn your first Energy", check_new_beginnings);
         this.addNewAchievement("Chain Reaction", "Get a continuous chain reaction of more than 2000 atoms", check_chain_reaction);
         this.addNewAchievement("Reactor Incremental", "Get your first Heat Point", check_reactor_incremental);
-        this.addNewAchievement("Nine Cirlces of Hell", "Sacrifice a total of 9 times (each Sacrifice should be for at least 1 Heat Points)", check_nine_circles_of_hell);
+        this.addNewAchievement("Nine Circles of Hell", "Sacrifice a total of 9 times (each Sacrifice should be for at least 1 Heat Points)", check_nine_circles_of_hell);
         this.addNewAchievement("All It Takes Is A Spark", "Upgrade your Energy Multiplier 5 times", check_all_it_takes_is_a_spark, "start new games with 2 additional atoms");
         this.addNewAchievement("Confined Space", "Get a total of 50.000 explosions", check_confined_space, "reactor size increase is significantly delayed");
         this.addNewAchievement("Don't You Have Something Better To Do?", "Play the game a total of 4 hours", check_dont_you_have_something_better_to_do);
@@ -81,7 +81,7 @@ Achievements.prototype = {
         this.addNewAchievement("Positively Charged", "Get a total of 1 billion Energy", check_positively_charged);
         this.addNewAchievement("Rich Boi", "Get Enriched Atoms", null, "a lot more lenient Sacrifice formula for bigger Energy values");
         this.addNewAchievement("The Instigator", "Reach 200 million Energy in no more than two reactor clicks", check_the_instigator, "start every new run with 200 Energy");
-        this.addNewAchievement("Speedrun", "Reach 1 heat point in less than 100 explosions", check_speedrun, "you get twice as much heat points on sacrifice");
+        this.addNewAchievement("Speedrun", "Reach 1 Heat Point in fewer than 100 explosions", check_speedrun, "you get twice as many Heat Points on Sacrifice");
         this.addNewAchievement("The Burning Souls", "Sacrifice for a total of 666 Heat Points", check_the_burning_souls);
         this.addNewAchievement("Eight Bits of Heat", "Get more than 256 Heat Points in a single Sacrifice", null, "you get a very small bonus to your Energy and Heat output for every hour played");
         this.addNewAchievement("Chernobyl Disaster", "Start a reactor Meltdown", null);
@@ -92,7 +92,7 @@ Achievements.prototype = {
         this.addNewAchievement("Faster Than Light", "Upgrade your tickrate using Matter Units");
         this.addNewAchievement("Actual Insanity", "Upgrade both External Intake and Longer Explosions", null, "you start a run with 2000 Energy");
         this.addNewAchievement("Extremely Funny", "Get 69420 Heat Points", check_extremely_funny, "Energy Multiplier, Enriched Atoms and Efficient Extraction upgrades become *extremely* more effective on higher upgrade levels");
-        this.addNewAchievement("It's A Never Ending Cycle", "Destroy the reactor in less than 10 minutes since last time you destroyed the reactor");
+        this.addNewAchievement("It's A Never Ending Cycle", "Destroy the reactor in less than 10 minutes since last time you Destroyed the reactor");
         this.addNewAchievement("Most Valuable Powerplant", "Get to 1 Billion passive Energy generation per second during Meltdown", check_mvp);
         this.addNewAchievement("Heavy Duty", "Get a total of 24 Matter Units over all reactors", check_heavy_duty, "significantly improve Matter Unit formula");
         this.addNewAchievement("Extremely F- wait, didn't I already unlock this achievement?", "Get over 69420 Heat Points in a single Sacrifice", null, "Energy gain bonus that is almost linearly affected by the amount of unspent Matter Units (capped at ~1000 MU)");
@@ -108,8 +108,13 @@ Achievements.prototype = {
     load: function(){
         var saved = JSON.parse(localStorage.getItem("achievements"));
         for(var ach in saved){
-            this.setCompletion(ach, saved[ach]);
-            console.log(ach);
+            if (ach == "Nine Cirlces of Hell") ach = "Nine Circles of Hell";
+
+            try {
+                this.setCompletion(ach, saved[ach]);
+            } catch (e) {
+
+            }
         }
     },
     
